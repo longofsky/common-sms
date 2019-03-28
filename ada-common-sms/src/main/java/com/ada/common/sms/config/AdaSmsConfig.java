a4e7f50644bc6308269abfc99fde89ff15370ada
tiantian <tiantian.li@adachina.com>
2019-03-28 21:31:04 +0800

    更新

---
+++ ada-common-sms/src/main/java/com/ada/common/sms/config/AdaSmsConfig.java
@@ -0,0 +1,19 @@
+package com.ada.common.sms.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ProjectName: microservice-spring-cloud
+ * @Package: com.ada.common.sms.config
+ * @ClassName: AdaSmsConfig
+ * @Author: litianlong
+ * @Description: ${description}
+ * @Date: 2019-03-28 21:27
+ * @Version: 1.0
+ */
+
+@Configuration
+public class AdaSmsConfig {
+
+
+}
